2e7bae189c2d3fab42b1267a6b728b42ae216659
saish98 <saish98@gmail.com>
2018-07-28 14:13:03 +0530

    first commit

---
+++ YLogging.podspec
@@ -0,0 +1,20 @@
+Pod::Spec.new do |s|
+#1.
+s.name               = "YLogging"
+#2.
+s.version            = "1.0.0"
+#3.
+s.summary         = "Sort description of 'YLogging' framework"
+#4.
+s.homepage        = "http://www.yudiz.com"
+#5.
+s.license              = "MIT"
+#6.
+s.author               = "AKanjariya"
+#7.
+s.platform            = :ios, "10.0"
+#8.
+s.source              = { :git => "https://github.com/saish98/YLogging.git", :tag => "1.0.0" }
+#9.
+s.source_files     = "YLogging", "YLogging/**/*.{h,m,swift}"
+end
---
+++ YLogging/YLog.swift
@@ -0,0 +1,34 @@
+//
+//  YLog.swift
+//  YLogging
+//
+//  Created by Heady on 28/07/18.
+//  Copyright © 2018 Heady. All rights reserved.
+//
+
+import Foundation
+
+public class YLog {
+    //1.
+    private var isDebug: Bool!
+    
+    //2.
+    public init() {
+        self.isDebug = false
+    }
+    
+    //3.
+    public func setup(isDebug: Bool) {
+        self.isDebug = isDebug
+        print("Project is in Debug mode: \(isDebug)")
+    }
+    
+    //4.
+    public func YPrint<T>(value: T) {
+        if self.isDebug == true {
+            print(value)
+        } else {
+            //Do any stuff for production mode
+        }
+    }
+}
